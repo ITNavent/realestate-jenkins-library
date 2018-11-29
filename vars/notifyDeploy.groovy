c098bb54d7870d509b7737d1f6ebc7dd85e1c279
Bernardo Falese <bfalese@navent.com>
2018-11-29 16:14:30 -0300

    ubicacion slack msg

--- vars/notifyDeploy.groovy
+++ vars/notifyDeploy.groovy
@@ -4,9 +4,9 @@ def call(Map config) {
 	def gitRevision = config?.gitRevision ?: 'sin datos'
 	def gitBranch = config?.gitBranch ?: 'sin datos'
 	def newrelicAppName = config?.newrelicAppName ?: ''
-	def slackMessage = config?.slackMessage ?: "Deploy iniciado por ${BUILD_USER_ID} de ${newrelicAppName} finalizado el ${$BUILD_TIMESTAMP} revision: ${gitRevision} branch: ${gitBranch}"
 	withCredentials([file(credentialsId: 'newrelic_api_key', variable: 'NR_API_KEY')]) {
 		wrap([$class: 'BuildUser']) {
+			def slackMessage = config?.slackMessage ?: "Deploy iniciado por ${BUILD_USER_ID} de ${newrelicAppName} finalizado el ${$BUILD_TIMESTAMP} revision: ${gitRevision} branch: ${gitBranch}"
 			slackSend(color: slackColor, channel: slackChannel, message: slackMessage)
 			if(newrelicAppName != null && !''.equals(newrelicAppName)) {
 				def nameResponse = sh(script: 'curl -X GET "https://api.newrelic.com/v2/applications.json" -H "X-Api-Key:${NR_API_KEY}" -i -d "filter[name]=${newrelicAppName}"', returnStdout: true)
