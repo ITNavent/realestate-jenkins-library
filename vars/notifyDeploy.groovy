9ecc5a2d6d1940255e712ad2052e6ae857e8e49d
Bernardo Falese <bfalese@navent.com>
2018-11-29 16:44:43 -0300

    comillas

--- vars/notifyDeploy.groovy
+++ vars/notifyDeploy.groovy
@@ -9,13 +9,13 @@ def call(Map config) {
 			def slackMessage = config?.slackMessage ?: "Deploy iniciado por ${BUILD_USER_ID} de ${newrelicAppName} finalizado el ${BUILD_TIMESTAMP} revision: ${gitRevision} branch: ${gitBranch}"
 			slackSend(color: slackColor, channel: slackChannel, message: slackMessage)
 			if(newrelicAppName != null && !''.equals(newrelicAppName)) {
-				def nameResponse = sh(script: 'curl -X GET "https://api.newrelic.com/v2/applications.json" -H "X-Api-Key:${NR_API_KEY}" -i -d "filter[name]=${newrelicAppName}"', returnStdout: true)
+				def nameResponse = sh(script: "curl -X GET 'https://api.newrelic.com/v2/applications.json' -H 'X-Api-Key:${NR_API_KEY}' -i -d 'filter[name]=${newrelicAppName}'", returnStdout: true)
 				def jsonResponse = readJSON text: "${nameResponse}"
 				def newrelicAppId = jsonResponse.applications[0].id
 				sh """
 				curl -X POST -H 'Content-Type: application/json' \
 				-H 'X-Api-Key:${NR_API_KEY}' -i \
-				-d \'{"deployment": { "revision": "${revision}", "user": "${BUILD_USER_ID}" } }\' https://api.newrelic.com/v2/applications/${newrelicAppId}/deployments.json
+				-d \'{"deployment": { "revision": "${gitRevision}", "user": "${BUILD_USER_ID}" } }\' https://api.newrelic.com/v2/applications/${newrelicAppId}/deployments.json
         		"""
 			}
 		}
