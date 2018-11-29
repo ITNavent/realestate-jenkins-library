764ac708fb2f64a4f91c09f8d87ff42296a5b1ba
Bernardo Falese <bfalese@navent.com>
2018-11-29 16:12:24 -0300

    nr app id

--- vars/notifyDeploy.groovy
+++ vars/notifyDeploy.groovy
@@ -11,11 +11,11 @@ def call(Map config) {
 			if(newrelicAppName != null && !''.equals(newrelicAppName)) {
 				def nameResponse = sh(script: 'curl -X GET "https://api.newrelic.com/v2/applications.json" -H "X-Api-Key:${NR_API_KEY}" -i -d "filter[name]=${newrelicAppName}"', returnStdout: true)
 				def jsonResponse = readJSON text: "${nameResponse}"
-				def newrelicAppName = jsonResponse.applications[0].id
+				def newrelicAppId = jsonResponse.applications[0].id
 				sh """
 				curl -X POST -H 'Content-Type: application/json' \
 				-H 'X-Api-Key:${NR_API_KEY}' -i \
-				-d \'{"deployment": { "revision": "${revision}", "user": "${BUILD_USER_ID}" } }\' https://api.newrelic.com/v2/applications/${newrelicAppName}/deployments.json
+				-d \'{"deployment": { "revision": "${revision}", "user": "${BUILD_USER_ID}" } }\' https://api.newrelic.com/v2/applications/${newrelicAppId}/deployments.json
         		"""
 			}
 		}
