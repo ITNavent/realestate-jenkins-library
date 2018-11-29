e4f8a7c5c2c3648924115453cc3801d811d4b7f4
Bernardo Falese <bfalese@navent.com>
2018-11-29 17:21:41 -0300

    sin headers

--- vars/notifyDeploy.groovy
+++ vars/notifyDeploy.groovy
@@ -9,11 +9,11 @@ def call(Map config) {
 			def slackMessage = config?.slackMessage ?: "Deploy por ${BUILD_USER_ID} para ${newrelicAppName} finalizado el ${BUILD_TIMESTAMP} revision: ${gitRevision} branch: ${gitBranch}"
 			slackSend(color: slackColor, channel: slackChannel, message: slackMessage)
 			if(newrelicAppName != null && !''.equals(newrelicAppName)) {
-				def nameResponse = sh(script: "curl -X GET 'https://api.newrelic.com/v2/applications.json' -H 'X-Api-Key:${NR_API_KEY}' -i -d 'filter[name]=${newrelicAppName}'", returnStdout: true)
-				echo nameResponse
-				def slurper = new groovy.json.JsonSlurper()
-				def jsonResponse = slurper.parseText(nameResponse)
-				//def jsonResponse = readJSON text: nameResponse
+				def nameResponse = sh(script: "curl -X GET 'https://api.newrelic.com/v2/applications.json' -H 'X-Api-Key:${NR_API_KEY}' -d 'filter[name]=${newrelicAppName}'", returnStdout: true)
+				//echo nameResponse
+				//def slurper = new groovy.json.JsonSlurper()
+				//def jsonResponse = slurper.parseText(nameResponse)
+				def jsonResponse = readJSON text: nameResponse
 				def newrelicAppId = jsonResponse.applications[0].id
 				sh """
 				curl -X POST -H 'Content-Type: application/json' \
