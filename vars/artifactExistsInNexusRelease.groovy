c3e6d784aa0b25e077ea6569db7bd4ff46c55e86
Bernardo Falese <bfalese@navent.com>
2018-12-06 09:42:19 -0300

    a

--- vars/artifactExistsInNexusRelease.groovy
+++ vars/artifactExistsInNexusRelease.groovy
@@ -1,5 +1,5 @@
 def call(String artifactName, String artifactVersion, String packagingType) {
-	def safePackagingType = packagingType? packagingType ?: 'jar'
+	def safePackagingType = packagingType == null? 'jar' : packagingType
 	def statusCode = sh(script: "curl -sL -w '%{http_code}' -o /dev/null -I 'http://nexus.navent.biz:8081/nexus/content/repositories/releases/com/navent/realestate/${artifactName}/${artifactVersion}/${artifactName}-${artifactVersion}.${safePackagingType}'", returnStdout: true)
 	return "200" == statusCode
 }
