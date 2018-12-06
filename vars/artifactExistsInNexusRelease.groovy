6a36cb12259b6ed85435b798d6a8780cbf3fa401
Bernardo Falese <bfalese@navent.com>
2018-12-06 09:47:48 -0300

    default param

--- vars/artifactExistsInNexusRelease.groovy
+++ vars/artifactExistsInNexusRelease.groovy
@@ -1,6 +1,5 @@
-def call(String artifactName, String artifactVersion, String packagingType) {
-	def safePackagingType = packagingType == null? 'jar' : packagingType
-	def statusCode = sh(script: "curl -sL -w '%{http_code}' -o /dev/null -I 'http://nexus.navent.biz:8081/nexus/content/repositories/releases/com/navent/realestate/${artifactName}/${artifactVersion}/${artifactName}-${artifactVersion}.${safePackagingType}'", returnStdout: true)
+def call(String artifactName, String artifactVersion, String packagingType = 'jar') {
+	def statusCode = sh(script: "curl -sL -w '%{http_code}' -o /dev/null -I 'http://nexus.navent.biz:8081/nexus/content/repositories/releases/com/navent/realestate/${artifactName}/${artifactVersion}/${artifactName}-${artifactVersion}.${packagingType}'", returnStdout: true)
 	return "200" == statusCode
 }
 
