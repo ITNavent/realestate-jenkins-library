f1742325fb6d19575d13313faf32e22e19d71a1c
Bernardo Falese <bfalese@navent.com>
2018-12-06 12:30:18 -0300

    trim

--- vars/imageExistsInContainerRegistry.groovy
+++ vars/imageExistsInContainerRegistry.groovy
@@ -1,5 +1,5 @@
 def call(String imageName, String tagName, String projectName = 'redeo-all') {
 	def count = sh(script: "gcloud container images list-tags --filter='tags=(${tagName})' --format='table[no-heading](digest)' 'gcr.io/${projectName}/${imageName}' | wc -l", returnStdout: true)
 	echo count
-	return !("0" == count) 
+	return !("0" == count.trim()) 
 } 
