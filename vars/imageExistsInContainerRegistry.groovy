43460995a59a876cb1efec44518ce9c6059ae738
Bernardo Falese <bfalese@navent.com>
2018-12-06 14:19:43 -0300

    creds

--- vars/imageExistsInContainerRegistry.groovy
+++ vars/imageExistsInContainerRegistry.groovy
@@ -1,5 +1,9 @@
 def call(String imageName, String tagName, String projectName = 'redeo-all') {
-	def count = sh(script: "gcloud container images list-tags --filter='tags=(${tagName})' --format='table[no-heading](digest)' 'gcr.io/${projectName}/${imageName}' | wc -l", returnStdout: true)
-	echo count
-	return !("0" == count.trim()) 
+	withCredentials([file(credentialsId: 'jenkins-redeo-all', variable: 'CHECK_PULL_KEYFILE')]) {
+		sh """
+		gcloud auth activate-service-account --key-file=${CHECK_PULL_KEYFILE} --project ${projectName}
+		"""
+		def count = sh(script: "gcloud container images list-tags --filter='tags=(${tagName})' --format='table[no-heading](digest)' 'gcr.io/${projectName}/${imageName}' | wc -l", returnStdout: true)
+		return !("0" == count.trim())
+	}
 } 
