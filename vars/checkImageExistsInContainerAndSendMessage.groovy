17f020d7cbb9e432476878d415193978cc96914f
luchammoe <lhammoe@navent.com>
2019-05-30 12:16:26 -0300

    nueva funcion para simplificar el chequeo de imagen de docker y enviar mensaje slack para todos los portales

---
+++ vars/checkImageExistsInContainerAndSendMessage.groovy
@@ -0,0 +1,10 @@
+
+def call(String imageName, String tagName, String newRelicProjectNameToShow, String projectName = 'redeo-all') {
+    IMAGE_EXISTS = imageExistsInContainerRegistry("${imageName}", "${tagName}")
+    if (!IMAGE_EXISTS) {
+        def MSG = "Imagen docker gcr.io/${projectName}/${imageName}:${tagName} no encontrada para deploy ${newRelicProjectNameToShow} - prd. No se ha deployado la version indicada. ${BUILD_URL}/console"
+        slackSend(color: 'danger', channel: '#deploys-realestate', message: MSG, title: "${newRelicProjectNameToShow} - prd")
+        currentBuild.result = 'ABORTED'
+        error(MSG)
+    }
+}
