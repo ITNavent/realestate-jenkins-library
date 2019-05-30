19de3d4b9a54e806b671957632302167451441e4
luchammoe <lhammoe@navent.com>
2019-05-30 14:15:01 -0300

    Vuelvo el channel al que corresponde

--- vars/checkImageExistsInContainerAndSendMessage.groovy
+++ vars/checkImageExistsInContainerAndSendMessage.groovy
@@ -3,7 +3,7 @@ def call(String imageName, String tagName, String newRelicProjectNameToShow, Str
     IMAGE_EXISTS = imageExistsInContainerRegistry("${imageName}", "${tagName}")
     if (!IMAGE_EXISTS) {
         def MSG = "Imagen docker gcr.io/${projectName}/${imageName}:${tagName} no encontrada para deploy ${newRelicProjectNameToShow} - prd. No se ha deployado la version indicada. ${BUILD_URL}/console"
-        slackSend(color: 'danger', channel: '#test-jenkins', message: MSG, title: "${newRelicProjectNameToShow} - prd")
+        slackSend(color: 'danger', channel: '#deploys-realestate', message: MSG, title: "${newRelicProjectNameToShow} - prd")
         currentBuild.result = 'ABORTED'
         error(MSG)
     }
