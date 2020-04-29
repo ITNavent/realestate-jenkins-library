f5a73fc27ca58f8d9773be247a24b53f99a302b0
Bernardo Falese <bfalese@navent.com>
2020-04-29 16:32:17 -0300

    cambio a funcion que te indica que color deployar si puede

--- vars/getBlueGreenDeploymentColour.groovy
+++ vars/getBlueGreenDeploymentColour.groovy
@@ -1,7 +1,9 @@
 def call(String statefulsetName, String vsName, String namespace, String clusterName, String projectName = 'redeo-all') {
 	def BLUE_REPLICAS = 0
 	def GREEN_REPLICAS = 0
-	def COLOR_DESC = ""
+	def COLOR_PARAM = ""
+	def VS_MAP = ['blue': 0,
+		'green': 0]
 	withCredentials([file(credentialsId: "jenkins-${projectName}", variable: 'CHECK_PULL_KEYFILE')]) {
 		sh """
 		gcloud auth activate-service-account --key-file=${CHECK_PULL_KEYFILE} --project ${projectName}
@@ -16,22 +18,27 @@ def call(String statefulsetName, String vsName, String namespace, String cluster
             GREEN_REPLICAS = sh(returnStdout: true, script: "kubectl get statefulset ${statefulsetName}-green -n ${namespace} -o jsonpath={.status.currentReplicas}")
         } catch(err) {}
         def VS_JSON = sh(returnStdout: true, script: "kubectl get virtualservice ${vsName} -n ${namespace} -o json")
-        // echo VS_JSON.toString()
         def VS_PROPS = readJSON text: VS_JSON
         def VS_ROUTES = VS_PROPS.spec.http[0].route
-        // echo BLUE_REPLICAS.toString()
-        // echo GREEN_REPLICAS.toString()
-        def VS_DESC = ""
         for(route in VS_ROUTES) {
-            VS_DESC += " " + route.destination.subset + ":" + route.weight.toString()
+            VS_MAP[route.destination.subset] = route.weight
         }
-        COLOR_DESC = "Replicas blue: ${BLUE_REPLICAS}\n Replicas green: ${GREEN_REPLICAS}\n Virtual service: ${VS_DESC}"
 	}
-	timeout(time: 60, unit: 'SECONDS') {
-		script {
-			def COLOR_PARAM = input(message: COLOR_DESC, ok: 'Next',
-				parameters: [choice(name: 'COLOR', choices: ['blue','green'].join('\n'), description: 'Seleccione color a deployar')])
-			return COLOR_PARAM;
-		}
+	script {
+		ansiColor('xterm') {
+            echo "\033[44m Blue replicas ${BLUE_REPLICAS} Blue virtual service ${VS_MAP['blue']} pct \033[0m"
+            echo "\033[42m Green replicas ${GREEN_REPLICAS} Green virtual service ${VS_MAP['green']} pct \033[0m"
+            if(BLUE_REPLICAS != 0 && GREEN_REPLICAS == 0 && VS_MAP['blue'] == 100 && VS_MAP['green'] == 0) {
+                COLOR_PARAM = 'green'
+                echo "\033[42m Deploy Green \033[0m"
+            } else if(GREEN_REPLICAS != 0 && BLUE_REPLICAS == 0 && VS_MAP['blue'] == 0 && VS_MAP['green'] == 100) {
+                COLOR_PARAM = 'blue'
+                echo "\033[44m Deploy Blue \033[0m"
+            } else {
+                currentBuild.result = 'ABORTED'
+                error('No se pudo decidir a que color debería desplegarse.')
+            }
+        }
+		return COLOR_PARAM;
 	}
 } 
