beac5ddd978036cd566213daaefcedd3a18f2359
Bernardo Falese <bfalese@navent.com>
2020-04-30 17:13:16 -0300

    baja al cambiar trafico

--- vars/updateVirtualService.groovy
+++ vars/updateVirtualService.groovy
@@ -1,20 +1,24 @@
-def call(String releaseName, String namespace, String colour, String chartLocation = './deploy/istio', String extraValues = "") {
-	def BLUE_REPLICAS = 0
-	def GREEN_REPLICAS = 0
-	def COLOR_PARAM = ""
-	def VS_MAP = ["blue": 0,
-		"green": 0]
+def call(String statefulsetName, String releaseName, String namespace, String colour, String chartLocation = './deploy/istio', String extraValues = "") {
+	def DOWN_COLOR = ""
+	def VS_MAP = ["blue": 0, "green": 0]
 	script {
-		if(colour != "blue" && colour != "green") {
+		if(colour == "blue") {
+			VS_MAP[colour] = 100
+			DOWN_COLOR = "green"
+		}
+		else if(colour == "green") {
+			VS_MAP[colour] = 100
+			DOWN_COLOR = "blue"
+		} else {
 			currentBuild.result = 'ABORTED'
 			error("'${colour}' no es un color de deploy valido, debe ser uno de ['blue', 'green']")
 		}
-		VS_MAP[colour] = 100
 		echo VS_MAP.toString()
 	}
 	sh """
 		helm upgrade --install ${releaseName}-istio ${chartLocation} ${extraValues} \
 		--namespace ${namespace} --set istio.blueWeight=${VS_MAP["blue"]} \
 		--set istio.greenWeight=${VS_MAP["green"]} --set istio.blueGreen=true --atomic
+		kubectl scale --replicas=0 statefulset/${statefulsetName}-${DOWN_COLOR} -n ${namespace}
 	"""
 }
