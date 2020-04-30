1c87a506fc017a482bcddcbaa873f03b10a9aa49
Bernardo Falese <bfalese@navent.com>
2020-04-30 10:56:10 -0300

    comillas dobles

--- vars/getBlueGreenDeploymentColour.groovy
+++ vars/getBlueGreenDeploymentColour.groovy
@@ -2,8 +2,8 @@ def call(String statefulsetName, String vsName, String namespace, String cluster
 	def BLUE_REPLICAS = 0
 	def GREEN_REPLICAS = 0
 	def COLOR_PARAM = ""
-	def VS_MAP = ['blue': 0,
-		'green': 0]
+	def VS_MAP = ["blue": 0,
+		"green": 0]
 	withCredentials([file(credentialsId: "jenkins-${projectName}", variable: 'CHECK_PULL_KEYFILE')]) {
 		sh """
 		gcloud auth activate-service-account --key-file=${CHECK_PULL_KEYFILE} --project ${projectName}
@@ -21,17 +21,18 @@ def call(String statefulsetName, String vsName, String namespace, String cluster
         def VS_PROPS = readJSON text: VS_JSON
         def VS_ROUTES = VS_PROPS.spec.http[0].route
         for(route in VS_ROUTES) {
-            VS_MAP[route.destination.subset] = route.weight
+            VS_MAP[route.destination.subset] = route.weight ?: 0
         }
+		echo VS_MAP
 	}
 	script {
 		ansiColor('xterm') {
             echo "\033[44m Blue replicas ${BLUE_REPLICAS} Blue virtual service ${VS_MAP['blue']} pct \033[0m"
             echo "\033[42m Green replicas ${GREEN_REPLICAS} Green virtual service ${VS_MAP['green']} pct \033[0m"
-            if(BLUE_REPLICAS != 0 && GREEN_REPLICAS == 0 && VS_MAP['blue'] == 100 && VS_MAP['green'] == 0) {
+            if(BLUE_REPLICAS != 0 && GREEN_REPLICAS == 0 && VS_MAP["blue"] == 100 && VS_MAP["green"] == 0) {
                 COLOR_PARAM = 'green'
                 echo "\033[42m Deploy Green \033[0m"
-            } else if(GREEN_REPLICAS != 0 && BLUE_REPLICAS == 0 && VS_MAP['blue'] == 0 && VS_MAP['green'] == 100) {
+            } else if(GREEN_REPLICAS != 0 && BLUE_REPLICAS == 0 && VS_MAP["blue"] == 0 && VS_MAP["green"] == 100) {
                 COLOR_PARAM = 'blue'
                 echo "\033[44m Deploy Blue \033[0m"
             } else {
--- vars/updateVirtualService.groovy
+++ vars/updateVirtualService.groovy
@@ -2,18 +2,19 @@ def call(String releaseName, String namespace, String colour, String chartLocati
 	def BLUE_REPLICAS = 0
 	def GREEN_REPLICAS = 0
 	def COLOR_PARAM = ""
-	def VS_MAP = ['blue': 0,
-		'green': 0]
+	def VS_MAP = ["blue": 0,
+		"green": 0]
 	script {
 		if(colour != "blue" && colour != "green") {
 			currentBuild.result = 'ABORTED'
 			error("'${colour}' no es un color de deploy valido, debe ser uno de ['blue', 'green']")
 		}
 		VS_MAP[colour] = 100
+		echo VS_MAP
 	}
 	sh """
 		helm upgrade --install ${releaseName}-istio ${chartLocation} --reuse-values \
-		--namespace ${namespace} --set istio.blueWeight=${VS_MAP['blue']} \
-		--set istio.greenWeight=${VS_MAP['green']} --set istio.blueGreen=true --atomic
+		--namespace ${namespace} --set istio.blueWeight=${VS_MAP["blue"]} \
+		--set istio.greenWeight=${VS_MAP["green"]} --set istio.blueGreen=true --atomic
 	"""
 }
