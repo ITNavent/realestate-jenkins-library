0576525643a5bd773fcf7449e3c1a359ea800cb4
Bernardo Falese <bfalese@navent.com>
2020-04-30 09:20:06 -0300

    comparacion strings

--- vars/updateVirtualService.groovy
+++ vars/updateVirtualService.groovy
@@ -5,7 +5,7 @@ def call(String releaseName, String namespace, String colour, String chartLocati
 	def VS_MAP = ['blue': 0,
 		'green': 0]
 	script {
-		if(colour != 'blue' || colour != 'green') {
+		if(colour != "blue" || colour != "green") {
 			currentBuild.result = 'ABORTED'
 			error("'${colour}' no es un color de deploy valido, debe ser uno de ['blue', 'green']")
 		}
@@ -14,6 +14,6 @@ def call(String releaseName, String namespace, String colour, String chartLocati
 	sh """
 		helm upgrade --install ${releaseName}-istio ${chartLocation} --reuse-values \
 		--namespace ${namespace} --set istio.blueWeight=${VS_MAP['blue']} \
-		--set istio.greenWeight=${VS_MAP['green']} --set istio.blueGreen=true
+		--set istio.greenWeight=${VS_MAP['green']} --set istio.blueGreen=true --atomic
 	"""
 }
