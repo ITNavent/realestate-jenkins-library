40d47befda4dd0a9216084eea3e5ce8acc877225
Bernardo Falese <bfalese@navent.com>
2020-05-05 15:39:33 -0300

    timeout y checkout parcial

--- vars/revertBlueGreenDeployment.groovy
+++ vars/revertBlueGreenDeployment.groovy
@@ -19,12 +19,19 @@ def call(String statefulsetName, String releaseName, String vsName, String names
 		checkout([
 			$class: 'GitSCM',
 			branches: [[name: "refs/tags/${DOWN_TAG_NAME}"]],
-			userRemoteConfigs: [[credentialsId: 'e9943f17-aaf0-4838-bb08-f747e259c0e9', url: "git@github.com:ITNavent/${gitProject}.git"]]
+			extensions: [[
+				$class: 'SparseCheckoutPaths',
+				sparseCheckoutPaths: [[path: "${chartLocation}/*"]]
+			]]
+			userRemoteConfigs: [[
+				credentialsId: 'e9943f17-aaf0-4838-bb08-f747e259c0e9', 
+				url: "git@github.com:ITNavent/${gitProject}.git"
+			]]
 		])
 		sh """
 		kubectl scale --replicas=${DOWN_REPLICAS} statefulset/${statefulsetName}-${UP_COLOR} -n ${namespace}
-		kubectl wait-sts ${statefulsetName}-${UP_COLOR} -n ${namespace}
+		kubectl wait-sts ${statefulsetName}-${UP_COLOR} -n ${namespace} --timeout 5m
 		"""
-		updateVirtualServiceReuseValues(statefulsetName, releaseName, namespace, UP_COLOR, chartLocation)
+		updateVirtualServiceReuseValues(statefulsetName, releaseName, namespace, UP_COLOR, "." + chartLocation)
 	}
 }
