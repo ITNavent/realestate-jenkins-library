e1ce5c67ffede1d5eb10ab85710e910c575dafef
Bernardo Falese <bfalese@navent.com>
2020-05-05 16:36:36 -0300

    parametros chart

--- vars/revertBlueGreenDeployment.groovy
+++ vars/revertBlueGreenDeployment.groovy
@@ -20,9 +20,7 @@ def call(String statefulsetName, String releaseName, String vsName, String names
 			$class: 'GitSCM',
 			branches: [[name: "refs/tags/${DOWN_TAG_NAME}"]],
 			extensions: [
-				[$class: 'CleanBeforeCheckout'],
-				[$class: 'SparseCheckoutPaths',
-				sparseCheckoutPaths: [[path: "${chartLocation}/*"]]
+				[$class: 'CleanBeforeCheckout']
 			]],
 			userRemoteConfigs: [[
 				credentialsId: 'e9943f17-aaf0-4838-bb08-f747e259c0e9', 
@@ -33,6 +31,6 @@ def call(String statefulsetName, String releaseName, String vsName, String names
 		kubectl scale --replicas=${DOWN_REPLICAS} statefulset/${statefulsetName}-${UP_COLOR} -n ${namespace}
 		kubectl wait-sts ${statefulsetName}-${UP_COLOR} -n ${namespace} --timeout 5m
 		"""
-		updateVirtualServiceReuseValues(statefulsetName, releaseName, namespace, UP_COLOR, "." + chartLocation)
+		updateVirtualServiceReuseValues(statefulsetName, releaseName, namespace, UP_COLOR, chartLocation)
 	}
 }
