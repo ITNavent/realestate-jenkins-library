394190cdff44a46f64796046b1827dfc53b0efb3
Bernardo Falese <bfalese@navent.com>
2020-05-13 12:40:05 -0300

    nombre app

--- vars/checkLostCommit.groovy
+++ vars/checkLostCommit.groovy
@@ -3,7 +3,7 @@ def call(String sha1Commit, String newRelicProjectNameToShow, String baseCommit 
 		def IS_ANCESTOR = sh(script: "git merge-base --is-ancestor ${baseCommit} ${sha1Commit} && echo yes || echo no", returnStdout: true)
 		echo "IS_ANCESTOR " + IS_ANCESTOR
 		if(IS_ANCESTOR != "yes") {
-			def msg = "El commit ${sha1Commit} no es ancestro de ${baseCommit}, deteniendo jenkins job."
+			def msg = "El commit ${sha1Commit} no es ancestro de ${baseCommit}, deteniendo jenkins job en ${newRelicProjectNameToShow}."
 			slackSend(color: 'danger', channel: channelId, message: msg, title: "${newRelicProjectNameToShow}")
 			currentBuild.result = 'ABORTED'
 			error(msg)
