ff2b6e76d9efdcd61fdcaab6b6c805177eae1fe3
Bernardo Falese <bfalese@navent.com>
2020-05-14 16:11:25 -0300

    echo

--- vars/countDeploymentFrequency.groovy
+++ vars/countDeploymentFrequency.groovy
@@ -2,11 +2,13 @@ def call() {
 	script {
 		def fields = [:]
 		fields['change_sucess'] = 1
+		echo "fields " + fields.toString()
 		def tags = [:]
 		tags['job_name']      = env.JOB_NAME
 		tags['build_number']  = env.BUILD_NUMBER
 		tags['git_commit']    = env.GIT_COMMIT
 		tags['git_branch']    = env.GIT_BRANCH
+		echo "tags " + tags.toString();
 		influxDbPublisher(customData: fields, customDataTags: tags, selectedTarget: 'influxdb-redeoall', measurementName: 'deploy')
 	}
 }
