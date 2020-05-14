cca737ddb7ce0c284f7e384eaed268a4c6f6ca2c
Bernardo Falese <bfalese@navent.com>
2020-05-14 15:50:44 -0300

    cuenta deploys

---
+++ vars/countDeploymentFrequency.groovy
@@ -0,0 +1,10 @@
+def call() {
+	def fields = [:]
+	fields['change_sucess'] = 1
+	def tags = [:]
+	tags['job_name']      = env.JOB_NAME
+	tags['build_number']  = env.BUILD_NUMBER
+	tags['git_commit']    = env.GIT_COMMIT
+	tags['git_branch']    = env.GIT_BRANCH
+	influxDbPublisher(customData: fields, customDataTags: tags, selectedTarget: 'influxdb-redeoall', measurementName: 'deploy')
+}
