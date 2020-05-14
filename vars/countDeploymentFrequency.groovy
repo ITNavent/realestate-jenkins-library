55929b24338f6edf5890c96d101158e01554228e
Bernardo Falese <bfalese@navent.com>
2020-05-14 16:06:41 -0300

    dentro de script

--- vars/countDeploymentFrequency.groovy
+++ vars/countDeploymentFrequency.groovy
@@ -1,10 +1,12 @@
 def call() {
-	def fields = [:]
-	fields['change_sucess'] = 1
-	def tags = [:]
-	tags['job_name']      = env.JOB_NAME
-	tags['build_number']  = env.BUILD_NUMBER
-	tags['git_commit']    = env.GIT_COMMIT
-	tags['git_branch']    = env.GIT_BRANCH
-	influxDbPublisher(customData: fields, customDataTags: tags, selectedTarget: 'influxdb-redeoall', measurementName: 'deploy')
+	script {
+		def fields = [:]
+		fields['change_sucess'] = 1
+		def tags = [:]
+		tags['job_name']      = env.JOB_NAME
+		tags['build_number']  = env.BUILD_NUMBER
+		tags['git_commit']    = env.GIT_COMMIT
+		tags['git_branch']    = env.GIT_BRANCH
+		influxDbPublisher(customData: fields, customDataTags: tags, selectedTarget: 'influxdb-redeoall', measurementName: 'deploy')
+	}
 }
